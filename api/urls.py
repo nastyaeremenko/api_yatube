91491d2cc0ef9ee4d764e41165142adbf6ed5429
Anastasiya <nastya.eremenko@mail.ru>
2021-03-10 19:52:37 +0300

    Homework Sprint10 v2

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -23,6 +22,7 @@ router.register('follow', FollowViewSet)
 
 urlpatterns = [
     path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
     path('v1/', include(router.urls))
 ]
